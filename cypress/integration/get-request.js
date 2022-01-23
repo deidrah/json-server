46fdea377ddefc83e1d9ba8c54056ed2e9dcfb80
Deidrah <dominikastasik93@gmail.com>
2022-01-23 13:54:09 +0100

    add cypress and get-request test

---
+++ cypress/integration/get-request.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+describe('Get Request', () => {
+  let result
+  it('Validate status code of the /posts api', () => {
+    result = cy.request('http://localhost:3000/posts')
+    result.its('status').should('equal', 200)
+  })
+})
